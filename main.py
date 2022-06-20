9597841587f9eed4c18d514cea52f547b6f1812e
vladyahnovets <v.yahnavets@gmail.com>
2022-06-21 01:13:54 +0300

    Добавил клетки как класс и научился бесконечно его генерировать. Дальше привязать голод к еде и размножение.

--- main.py
+++ main.py
@@ -11,6 +11,43 @@ FONT = pygame.font.SysFont('conicsans', 16)
 
 BG_COLOR = (255, 255, 255)
 BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+
+random = 0
+cell_number = 0
+
+
+
+class Food:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def draw(self, win):
+        pygame.draw.circle(win, BLUE, (self.x, self.y), 2)
+
+    def update(self):
+        pass
+
+class Cell:
+    def __init__(self, x, y, hungry):
+        self.x = x
+        self.y = y
+        self.hungry = hungry
+
+    def move(self, win):
+        random = randint(-4, 4)
+        self.x = self.x + random
+        random = randint(-4, 4)
+        self.y = self.y + random
+        pygame.draw.circle(win, RED, (self.x, self.y), 10)
+        hungry_text = FONT.render(f'{self.hungry}', 1, BLUE)
+        win.blit(hungry_text, (self.x+8, self.y+8))
+
+    def status_update(self, list, name):
+        self.hungry = self.hungry + randint(-30, -10)
+        if self.hungry <= 0:
+            list.remove(name)
 
 
 def main():
@@ -21,7 +58,7 @@ def main():
     seconds = 0
 
     cells = []
-    cells.append([350, 350])
+    cells.append(Cell(350, 350, 100))
 
     while run:
         time = time + 1
@@ -36,17 +73,20 @@ def main():
                 run = False
 
 
-        for cell in range(len(cells)):
-            x = randint(-4, 4)
-            y = randint(-4, 4)
-            cells[cell] = [cells[cell][0] + x, cells[cell][1] + y]
-            pygame.draw.circle(WIN, BLUE, (cells[cell][0], cells[cell][1]), 2)
+        if seconds%1 == 0 and time%60 == 0:
+            imp_number = len(cells)
+            imp2_number = 0
+            for name in cells:
+                if imp2_number == imp_number:
+                    break
+                else:
+                    cells.append(Cell(name.x, name.y, 100))
+                imp2_number = imp2_number + 1
+        for name in cells:
+            name.move(WIN)
+            if seconds % 2 == 0 and time % 60 == 0:
+                name.status_update(cells, name)
 
-
-
-        if seconds%2 == 0 and time%60 == 0:
-            for i in range(len(cells)):
-                cells.append(cells[i])
 
         WIN.blit(FONT.render(f'{seconds} seconds, {len(cells)} creatures', 1, (0, 0, 0)), (50, 650))
         pygame.display.update()
