626fb9daa2516abce967174eb060e180a47369e6
vladyahnovets <v.yahnavets@gmail.com>
2022-06-23 16:08:52 +0300

    Сделал проверку на границы и поиск пути(мне кажется его можно реализовать лучше)

--- main.py
+++ main.py
@@ -34,12 +34,42 @@ class Cell:
         self.x = x
         self.y = y
         self.hungry = hungry
+        self.speed = 4
 
-    def move(self, win):
-        random = randint(-4, 4)
-        self.x = self.x + random
-        random = randint(-4, 4)
-        self.y = self.y + random
+
+    def move(self, win, food_list):
+
+        # Поиск пути к ближайшей еде
+        closest = [0, 9999999]
+        for name in food_list:
+            distance = (name.x - self.x) ** 2 + (name.y - self.y) ** 2
+            if closest[1] > distance:
+                closest[1] = distance
+                closest[0] = name
+        if self.x < closest[0].x:
+            self.x = self.x + self.speed
+        elif self.x > closest[0].x:
+            self.x = self.x - self.speed
+        else:
+            self.x = self.x + 0
+        if self.y < closest[0].y:
+            self.y = self.y + self.speed
+        elif self.y > closest[0].y:
+            self.y = self.y - self.speed
+        else:
+            self.y = self.y + 0
+
+        # Проверка на границы
+        if self.x <= 20:
+            self.x = self.x + 10
+        elif self.y <= 20:
+            self.y = self.y + 10
+        elif self.x >= 680:
+            self.x = self.x - 10
+        elif self.y >= 680:
+            self.y = self.y - 10
+
+        # Прорисовка клетки
         pygame.draw.circle(win, RED, (self.x, self.y), 10)
         hungry_text = FONT.render(f'{self.hungry}', 1, BLUE)
         win.blit(hungry_text, (self.x+8, self.y+8))
@@ -51,6 +81,7 @@ class Cell:
         if self.hungry >= 70:
             list.append(Cell(self.x, self.y, 50))
             self.hungry = self.hungry - 50
+
     def eat(self, cell, list_food):
         for name in list_food:
             # print(name.x, cell.x)
@@ -88,9 +119,9 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
-        if len(food) < 150:
-            imp_number = randint(150, 490)
-            imp2_number = randint(150, 490)
+        if len(food) < 50:
+            imp_number = randint(40, 660)
+            imp2_number = randint(40, 660)
             food.append(Food(imp_number, imp2_number))
 
 
@@ -98,9 +129,9 @@ def main():
             cells.append(Cell(350, 350, 100))
 
         for name in cells:
-            name.move(WIN)
+            name.move(WIN, food)
             name.eat(name, food)
-            if len(food) < 150:
+            if len(food) < 50:
                 i = i + 1
             if seconds % 2 == 0 and time % 60 == 0:
                 name.status_update(cells, name)
@@ -108,7 +139,7 @@ def main():
         for name in food:
             name.appear(WIN)
 
-        WIN.blit(FONT.render(f'{seconds} seconds, {len(cells)} creatures, {len(food)} food, {i-179}', 1, (0, 0, 0)), (50, 650))
+        WIN.blit(FONT.render(f'{seconds} seconds, {len(cells)} creatures, {len(food)} food, {i-49}', 1, (0, 0, 0)), (50, 650))
         pygame.display.update()
 
     pygame.quit()
